f1dcdedc339bc134222f59cce5e05ad2e3e6f6ff
Will Hodgson <will.hodgson@current-rms.com>
2019-10-17 10:16:12 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191017100717) do
+ActiveRecord::Schema.define(version: 20191017101411) do
 
   create_table "figures", force: :cascade do |t|
     t.string "name"
