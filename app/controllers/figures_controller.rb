f334caac24e59684c7f2819af009b09d4e7d3bfa
Will Hodgson <will.hodgson@current-rms.com>
2019-10-17 12:17:03 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,7 +1,10 @@
 class FiguresController < ApplicationController
+
   get '/figures/new' do
+    puts params
     @titles = Title.all
     @landmarks = Landmark.all
     erb :'/figures/new'
   end
+
 end
